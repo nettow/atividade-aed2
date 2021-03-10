1227fdbb6612c0b3c9907e99a6936684b688edcd
Neto <jlsnsilveira@sga.pucminas.br>
2021-03-10 18:37:56 -0300

    Alteração Aleatória

---
+++ AlteracaoAleatoria.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
+public class AlteracaoAleatoria {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String text = "";
+        while(!text.equals("FIM")){
+            System.out.println(alteracaoAleatoria(input.nextLine()));
+        }
+
+        input.close();
+    }
+
+    private static String alteracaoAleatoria(String text){
+        Random rnd = new Random();
+        String textWithRandomLetters = "";
+        rnd.setSeed(4);
+        int rnd1 = rnd.nextInt(25) + 97;
+        int rnd2 = rnd.nextInt(25) + 97;
+
+        System.out.println((char) rnd1 + "  " +  (char) + rnd2);
+        for(int i=0; i < text.length(); i++)
+            if((int)text.charAt(i) == rnd1)
+                textWithRandomLetters += (char)rnd2;
+            else
+                textWithRandomLetters += text.charAt(i);
+
+        return textWithRandomLetters;
+    }
+}
